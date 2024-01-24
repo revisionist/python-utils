f5a272db384361209fddda12db715dc6dbfa2479
David Goddard <dmg@parse.net>
2024-01-24 20:05:11 +0100

    Add Apache license headers

--- domestique/flask/response.py
+++ domestique/flask/response.py
@@ -1,3 +1,17 @@
+# Copyright 2023-2024 David Goddard.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain a
+# copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 import logging
 
 from datetime import datetime
@@ -124,7 +138,8 @@ class ResponseWrapper:
                 self.add_meta("client_id", self.client_id)
             self.add_meta("host", request.host)
             self.add_meta("path", request.path)
-            self.add_meta("method", self.method_text)
+            self.add_meta("method", request.method)
+            self.add_meta("handler", self.method_text)
             self.add_meta("timestamp_ms", epoch_ms)
             self.add_meta("timestamp_str", str(current_time))
             self.add_meta("execution_time_ms", execution_time_ms)
